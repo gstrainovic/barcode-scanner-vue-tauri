5e2aa89790a0c0099f2146e9edc389b152efd7d0
gstrainovic <g.strainovic@gmail.com>
2025-04-19 23:54:41 +0200

    pt fix

--- src/views/pages/Team.vue
+++ src/views/pages/Team.vue
@@ -20,9 +20,18 @@ const nextPage = () => {
 };
 
 </script>
+
+<style>
+html, body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+}
+</style>
+
 <template>
     <div
-        class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
+        class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center pt-[120px] w-full overflow-hidden">
         <div class="flex flex-col items-center justify-center">
             <div
                 style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
