de384e652ddff979f945d1a870c7373349882622
gstrainovic <g.strainovic@gmail.com>
2025-05-10 07:27:40 +0200

    entferne console logs

--- src/composables/helpers.ts
+++ src/composables/helpers.ts
@@ -7,12 +7,10 @@ export const onlineCheck = async () => {
         const response = await fetch(url, { method: 'GET' });
         const text = await response.text();
         if (response.status === 200 && text.includes('The server is running successfully')) {
-            console.log('Server is online');
             return true;
         }
     } catch (error) {
         console.error('An error occurred:', error);
     }
-    console.log('Server is offline');
     return false;
 }
