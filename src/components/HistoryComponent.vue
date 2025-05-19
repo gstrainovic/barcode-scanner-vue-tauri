bb28de2435bb807d93b2eae82a612cf123e324db
gstrainovic <g.strainovic@gmail.com>
2025-05-19 20:36:30 +0200

    history store and component

---
+++ src/components/HistoryComponent.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useHistoryStore } from '@/stores/historyStore';
+import { storeToRefs } from 'pinia';
+
+const historyStore = useHistoryStore();
+
+const { history } = storeToRefs(historyStore);
+
+onMounted(() => {
+    historyStore.loadHistory();
+});
+
+const statusClass = (status: string) => {
+    if (status.startsWith('@C03')) {
+        return 'text-orange-500';
+    } else if (status.startsWith('@C88')) {
+        return 'text-red-500';
+    } else {
+        return 'text-green-500';
+    }
+};
+
+const displayStatus = (status: string) => {
+    if (status.startsWith('@C03')) {
+        return status.replace('@C03', '');
+    } else if (status.startsWith('@C88')) {
+        return status.replace('@C88', '');
+    } else {
+        return status;
+    }
+};
+
+</script>
+
+<template>
+    <div class="flex flex-col w-1/2 mt-1">
+        <div class="table-container">
+            <DataTable :value="history" :sortField="'timestamp'" :sortOrder="-1" paginator :rows="5">
+                <template #header>
+                    <div class="flex flex-wrap items-center justify-between gap-2">
+                        <span class="text-xl font-bold">Verlauf</span>
+                    </div>
+                </template>
+                <Column field="status" header="Status" sortable style="width: 20%;font-size: 1.7rem;">
+                    <template #body="slotProps">
+                        <span :class="statusClass(slotProps.data.status)">{{
+                            displayStatus(slotProps.data.status)
+                        }}</span>
+                    </template>
+                </Column>
+                <Column field="barcode" header="Barcode" sortable style="width: 50%;font-size: 1.7rem"></Column>
+                <Column field="timestamp" header="Datum" sortable style="width: 30%;font-size: 1.7rem"></Column>
+            </DataTable>
+        </div>
+    </div>
+</template>
---
+++ src/stores/historyStore.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia';
+import { invoke } from '@tauri-apps/api/core';
+
+
+export const useHistoryStore = defineStore('history', {
+  state: () => ({
+    history: [] as { status: string; barcode: string; timestamp: string }[],
+  }),
+  actions: {
+    async loadHistory() {
+        this.history = await invoke<[]>('load_history');
+    }
+  },
+  persist: {
+    storage: sessionStorage, // Speichert den Zustand im sessionStorage
+  },
+});
