9bf987b07f5fdc1aa6b31ab050fbbf83c64975ea
gstrainovic <g.strainovic@gmail.com>
2025-05-28 09:16:46 +0200

    fixes

--- src-tauri/src/config/lib.rs
+++ src-tauri/src/config/lib.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 pub struct ApiConfig {
     pub strapi: String,
 }
@@ -14,7 +16,19 @@ pub struct Config {
 
 impl Config {
     pub fn from_env() -> Self {
+        // Prüfe ob .env Datei existiert,
+        // falls nein, lade es herunter von https://raw.githubusercontent.com/gstrainovic/barcode-scanner-vue-tauri/refs/heads/main/.env
+        // und speichere es im aktuellen Verzeichnis.
+        if !std::path::Path::new(".env").exists() {
+            let url = "https://raw.githubusercontent.com/gstrainovic/barcode-scanner-vue-tauri/refs/heads/main/.env";
+            let response = reqwest::blocking::get(url)
+                .expect("Failed to download .env file");
+            let content = response.text().expect("Failed to read response text");
+            fs::write(".env", content).expect("Failed to write .env file");
+        }
+
         dotenv::dotenv().ok();
+
         Self {
             api: ApiConfig {
                 strapi: std::env::var("VITE_STRAPI_URL").expect("VITE_STRAPI_URL not set"),
