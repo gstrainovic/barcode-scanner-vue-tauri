a0c5edf17078a4790fae07a6cd34fa90c9595233
gstrainovic <g.strainovic@gmail.com>
2025-07-06 21:22:19 +0200

    2.0.15 - release repo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Barcodescanner Client !
+# Barcodescanner V2 Client !
 Der Barcodescanner braucht keine besondere Einstellungen wie spezielle Sonderzeichen als Prefix
 oder verlangsamter Scannmodus etc.
 Neue BarcodeScanner müssen jedoch im Quellcode eingetragen werden!
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,3 +8,7 @@ echo "Neue Version: $NEW_VERSION"
 git tag v$NEW_VERSION
 git push origin v$NEW_VERSION
 gh release create v$NEW_VERSION ./src-tauri/target/release/$NEW_VERSION-barcode_scanner-x86_64-pc-windows-msvc.zip --notes v$NEW_VERSION
+
+# Zweites Release-Repo: gravurzeile/barcode-scanner-v2-releases
+git push git@github.com:gravurzeile/barcode-scanner-v2-releases.git v$NEW_VERSION
+gh release create v$NEW_VERSION ./src-tauri/target/release/$NEW_VERSION-barcode_scanner-x86_64-pc-windows-msvc.zip --notes v$NEW_VERSION --repo gravurzeile/barcode-scanner-v2-releases
